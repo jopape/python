3ee85373e05f316a14df5b0a171dda8f1210289c
Joao Paulo <paulojoao2005@yahoo.com.br>
2012-12-13 17:12:38 -0200

    django

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,21 @@
 from polls.models import Poll
+from polls.models import Choice
+
 from django.contrib import admin
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 2
+
 
 class PollAdmin(admin.ModelAdmin):
 	fields = ['pub_date','question']
-	
+	inlines = [ChoiceInline]
+	list_display = ('question','pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question','pub_date']
+	date_hierarchy = 'pub_date'
 admin.site.register(Poll,PollAdmin)
+
+admin.site.register(Choice)
 
